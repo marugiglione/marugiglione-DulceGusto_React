46f0ebe92a14db14f1308d701f10e8f6484d832c
marugiglione <maru.giglione@gmail.com>
2022-11-12 22:37:02 -0300

    Segunda Entrega Proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
+
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
+//import { useState } from 'react'
+
+import Navbar2 from './components/Navbar/Navbar'
+import ItemListContainer from './components/ItemListContainer/itemListContainer'
+import ItemDetailContainer from './pages/ItemDetailContainer/ItemDetailContainer'
+import Cart from './pages/Cart/Cart'
+
+// .css
 import './App.css'
-import Navbar from './components/Navbar'
-import ItemListContainer from './components/itemListContainer'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const saludo = 'saludando' // estado
+
+  const saludar = () => {
+      console.log('hola soy app')
+  } 
 
   return (
-    <div className="App">
-      <Navbar />
-      <ItemListContainer titulo = {'DULCE GUSTO'} subtitulo = {'Productos'}/>
-    </div>
+    <BrowserRouter>
+      <Navbar2/>
+      <Routes>
+        <Route path='/' element={<ItemListContainer greeting={saludo} saludar={ saludar } />} />
+        <Route path='/categoria/:categoriaId' element={<ItemListContainer greeting={saludo} saludar={ saludar } />} />
+        <Route path='/detail/:productId' element={<ItemDetailContainer />} />
+        <Route path='/cart'  element={<Cart />}/>
+        
+        <Route path='*' element={<Navigate to='/' />} />             
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
